c96761b348b6e75b7321476a274bf6a476dac871
Aric Crosson Bouwers <aricbouwers@outlook.com>
2023-04-15 18:00:24 -0600

    removing cors, and setting up proxy server properly instead.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import volunteerRouter from "./routes/volunteerRouter.js";
 import authenticationRouter from "./routes/signup.js";
-import cors from "cors";
 import bodyParser from "body-parser";
 
 //read the MongoDB credentials from .env file
@@ -26,20 +25,19 @@ const PORT = process.env.PORT || 9000;
 
 app.use(express.json()); // this tells our express app that we expect to be receiving json values.
 app.use(bodyParser.json());
-app.use(cors());
 //---- example json value being passed with curl as a post request:
 // curl -d '{"a":"hello","b":"bye"}
 // app.use(express.urlencoded() // another option to tell express app that we expect to be receiving url encoded values.(which usually come from online forms)
 //---- example urlencoded value being passed with curl as a post request:
 // curl =d "param1=value1&param2=value2" -X POST http://localhost:4444/aaa\?foo\=bar\&fred\=barney
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
     res.send("You reached the home endpoint for ConnectCause!.\n");
 });
 
 // list of the routers i'm using
-app.use("/volunteers", volunteerRouter); // all the functionality to do with volunteers
-app.use("/authenticate", authenticationRouter);
+app.use("/api/volunteers", volunteerRouter); // all the functionality to do with volunteers
+app.use("/api/authenticate", authenticationRouter);
 
 //port the server is listening on
 app.listen(PORT, () => {
