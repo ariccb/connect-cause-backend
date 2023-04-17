849acd4535748f63ea5c981054dd41f134fea5ec
Aric Crosson Bouwers <aricbouwers@outlook.com>
2023-04-17 16:27:21 -0600

    working hashing passwords linked with frontend signin form

--- models/volunteerSchema.js
+++ models/volunteerSchema.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt"; // this is a package that will encrypt the password. use npm install bcrypt to install it.
 import { cpSync } from "fs";
 
-
 /** this is the schema for the volunteers that sign up
  * to our application.
  */
@@ -12,45 +11,6 @@ const volunteerSchema = new mongoose.Schema({
     last_name: { type: String, required: true },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }, // i add this to the schema
-    created_at: { type: Date, default: new Date() },
-    birth_date: { type: Date, required: true }, // i add this to the schema
-    updated_at: { type: Date, required: false },
-    interests: [String], // add interests field as an array of strings
-  about_me: { type: String, required: false }, // add about_me field as a string
-  past_volunteer: { type: String, required: false } // add past_volunteer field as a string
-});
-    
-        
-
-//felmon is working on this password hashing part.
-// this code hash the password before saving it to the database. this is a middleware function that will run before the save function is called.
-// this is a pre hook. this code will run before the save function is called.
-    volunteerSchema.pre("save", async function (next) {
-    const volunteer = this  
-
-     // this code only hash the password if it has been modified or is new password, when there is an input to the password field.
- if (!volunteer.isModified("password")) {
-  return next();
-    }    
-
-    
-    // generate a salt and hash the password using the salt
-    // the salt is a random string that is added to the password before hashing it. this is to prevent rainbow table attacks.
-    // the salt is stored in the database along with the hashed password.
-   //rainbow table attack is a type of attack where the attacker has a list of pre-computed hashes and tries to match the hash of the password that they are trying to crack.
-   try { 
-const salt = await bcrypt.genSalt(10);
-const hash = await bcrypt.hash(volunteer.password, salt);
-
-    // replace the plaintext password with the hashed password and salt 
-    volunteer.password = hashedPassword;
-    next();
-  } catch (error) {
-    next(error);
-  }
-=======
-    // will need to implement some authentication / encryption to the password field - need to look into that
     password: { type: String, required: true },
     created_at: {
         type: Date,
@@ -75,16 +35,43 @@ const hash = await bcrypt.hash(volunteer.password, salt);
     },
     profile_image: { type: String, required: false },
     linkedin_link: { type: String, required: false },
->>>>>>> 05dd4fa87ca325a67dc20e5aa73630e0c0e5ebae
+    past_volunteer: { type: String, required: false }, // add past_volunteer field as a string
+});
+
+//felmon is working on this password hashing part.
+// this code hash the password before saving it to the database. this is a middleware function that will run before the save function is called.
+// this is a pre hook. this code will run before the save function is called.
+volunteerSchema.pre("save", async function (next) {
+    const volunteer = this;
+
+    // this code only hash the password if it has been modified or is new password, when there is an input to the password field.
+    if (!volunteer.isModified("password")) {
+        return next();
+    }
+
+    // generate a salt and hash the password using the salt
+    // the salt is a random string that is added to the password before hashing it. this is to prevent rainbow table attacks.
+    // the salt is stored in the database along with the hashed password.
+    //rainbow table attack is a type of attack where the attacker has a list of pre-computed hashes and tries to match the hash of the password that they are trying to crack.
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(volunteer.password, salt);
+
+        // replace the plaintext password with the hashed password and salt
+        volunteer.password = hashedPassword;
+        next();
+    } catch (error) {
+        next(error);
+    }
 });
 
 volunteerSchema.methods.isValidPassword = async function (password) {
-  const volunteer = this;
-  return await bcrypt.compare(password, volunteer.password);
+    const volunteer = this;
+    return await bcrypt.compare(password, volunteer.password);
 };
 
 // create the Volunteer model based on the schema above and export it to be saved in the database.
-const Volunteer = mongoose.model("volunteer", volunteerSchema); 
+const Volunteer = mongoose.model("volunteer", volunteerSchema);
 
 // export the Volunteer model
 export default Volunteer;
